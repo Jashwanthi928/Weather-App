9dbbe7cceb6045e13ca4d73812d77e4e1954cffd
Jashwanthi928 <54614090+Jashwanthi928@users.noreply.github.com>
2019-08-30 20:31:04 +0530

    Updated

--- weatherApp.js
+++ weatherApp.js
@@ -2,47 +2,26 @@
 function getTwelveHrs() {
     var today = new Date();
       var day = today.getDay();
+      var hourlist=[12,01,02,03,04,05,06,07,08,09,10,11,12,01,02,03,04,05,06,07,08,09,10,11];
       var daylist = ["Sunday","Monday","Tuesday","Wednesday ","Thursday","Friday","Saturday"];
       var hour = today.getHours();
       var minute = today.getMinutes();
-      var prepand = (hour >= 12)? " PM ":" AM ";
-      hour = (hour >= 12)? hour - 12: hour;
-      if (hour===0 && prepand===' PM ') 
-      { 
-      if (minute===0 && second===0)
-      { 
-      hour=12;
-      prepand=' Noon';
-      } 
-      else
-      { 
-      hour=12;
-      prepand=' PM';
-      } 
-      } 
-      if (hour===0 && prepand===' AM ') 
-      { 
-      if (minute===0 && second===0)
-      { 
-      hour=12;
-      prepand=' Midnight';
-      } 
-      else
-      { 
-      hour=12;
-      prepand=' AM';
-      } 
-      } 
-    var t= hour  +':'+ minute + prepand
-    var d = daylist[day] + " "
-    document.getElementById('daytime').innerHTML =d+' '+t;
+      var appand = (hour >= 12)? " pm ":" am ";
+      if(minute<10)
+      {
+        minute="0"+minute
+      }
+      else{minute=minute}
+    var time= hourlist[hour]  +':'+ minute + appand
+    var day = daylist[day] + " "
+    document.getElementById('daytime').innerHTML =day+' '+time;
     }
     function cToF(celsius) 
     {
       var cTemp = celsius;
       var cToFahr = cTemp * 9 / 5 + 32;
       var message =  cToFahr + ' \xB0F.';
-        document.getElementById('fah').innerHTML =message
+        document.getElementById('fah').innerHTML =message;
     }
     
     function fToC(fahrenheit) 
@@ -50,16 +29,22 @@ function getTwelveHrs() {
       var fTemp = fahrenheit;
       var fToCel = (fTemp - 32) * 5 / 9;
       var message =  fToCel + '\xB0C.';
-        document.getElementById('cel').innerHTML =message
+        document.getElementById('cel').innerHTML =message;
     } 
     cToF(60);
     fToC(45);
     function validateForm() {
-      var x = document.forms["cityName"]["input"].value;
-         return document.getElementById("City").innerHTML = x;
-         document.getElementById("sub").addEventListener("click", function(event){
-          event.preventDefault()
-        });
+      var x = document.getElementById("input").value;
+         return document.getElementById("city").innerHTML = x;
+        
     }
-    
-    
+    const msJashwanthi=[
+      {location:"Bangalore,karnataka",temp:23},
+      {location:"Hyderabad,Andhra pradesh",temp:34}
+    ]
+      function tempCall(){
+    var x = document.getElementById("input").value;
+      var Friday = msJashwanthi.filter(function(v) {if(v.location===x)return v });
+      return document.getElementById("cel").innerHTML = Friday[0].temp;
+      }
+      
